c00f91576b227405f2a268b354a2c50a09c082be
zakhap <zhs2104@columbia.edu>
2020-09-18 10:12:58 -0700

    Set up types.ts file structure

---
+++ src/marlin/types.ts
@@ -0,0 +1,37 @@
+import { Event } from 'ethers';
+
+
+
+
+
+
+export enum EntityKind {
+  Proposal = 'proposal',
+}
+
+export enum EventKind {
+  // MOLOCH EVENTS
+  // SubmitProposal = 'submit-proposal',
+  // SubmitVote = 'submit-vote',
+  // ProcessProposal = 'process-proposal',
+  // Ragequit = 'ragequit',
+  // Abort = 'abort',
+  // UpdateDelegateKey = 'update-delegate-key',
+  // SummonComplete = 'summon-complete',
+
+  // Marlin Events
+}
+
+interface IEvent {
+  kind: EventKind;
+}
+
+type Address = string;
+type Balance = string;
+
+// for each EventKind, create interface that extends IEvent
+
+
+
+
+// export type IEventData = IEachEventKind | IEachEventKind2...
