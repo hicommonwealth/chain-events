2c89d22fa5bafa7dc2efaa39813f73f2a4596f1e
zakhap <zhs2104@columbia.edu>
2020-09-23 10:42:37 -0700

    Remove unnecessary arguments from processor

--- src/marlin/processor.ts
+++ src/marlin/processor.ts
@@ -10,10 +10,8 @@
 // const log = factory.getLogger(formatFilename(__filename));
 
 // export class Processor extends IEventProcessor<Api, RawEvent> {
-//   private _version: 1 | 2;
 
 //   constructor(api: Api, 
-//     // contractVersion: 1 | 2
 //   ) {
 //     super(api);
 //     // this._version = contractVersion;
@@ -27,7 +25,7 @@
 //    * @returns an array of processed events
 //    */
 //   public async process(event: RawEvent): Promise<CWEvent<IEventData>[]> {
-//     const kind = ParseType(this._version, event.event);
+//     const kind = ParseType(event.event);
 //     if (!kind) return [];
 //     try {
 //       const cwEvent = await Enrich(this._version, this._api, event.blockNumber, kind, event);
