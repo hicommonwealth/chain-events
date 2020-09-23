4ac0040d04ced301b706efa0269918d0370fba95
zakhap <zhs2104@columbia.edu>
2020-09-23 15:14:59 -0700

    Fix processor

--- src/marlin/processor.ts
+++ src/marlin/processor.ts
@@ -1,38 +1,36 @@
-// /**
-//  * Processes Marlin events.
-//  */
-// import { IEventProcessor, CWEvent } from '../interfaces';
-// import { ParseType } from './filters/type_parser';
-// import { Enrich } from './filters/enricher';
+/**
+ * Processes Marlin events.
+ */
+import { IEventProcessor, CWEvent } from '../interfaces';
+import { ParseType } from './filters/type_parser';
+import { Enrich } from './filters/enricher';
 
-// import { IEventData, RawEvent, Api } from './types';
-// import { factory, formatFilename } from '../logging';
-// const log = factory.getLogger(formatFilename(__filename));
+import { IEventData, RawEvent, Api } from './types';
+import { factory, formatFilename } from '../logging';
+const log = factory.getLogger(formatFilename(__filename));
 
-// export class Processor extends IEventProcessor<Api, RawEvent> {
+export class Processor extends IEventProcessor<Api, RawEvent> {
 
-//   constructor(api: Api, 
-//   ) {
-//     super(api);
-//     // this._version = contractVersion;
-//   }
+  constructor(api: Api) {
+    super(api);
+  }
 
-//   /**
-//    * Parse events out of an ethereum block and standardizes their format
-//    * for processing.
-//    *
-//    * @param block the block received for processing
-//    * @returns an array of processed events
-//    */
-//   public async process(event: RawEvent): Promise<CWEvent<IEventData>[]> {
-//     const kind = ParseType(event.event);
-//     if (!kind) return [];
-//     try {
-//       const cwEvent = await Enrich(this._version, this._api, event.blockNumber, kind, event);
-//       return [ cwEvent ];
-//     } catch (e) {
-//       log.error(`Failed to enrich event: ${e.message}`);
-//       return [];
-//     }
-//   }
-// }
+  /**
+   * Parse events out of an ethereum block and standardizes their format
+   * for processing.
+   *
+   * @param block the block received for processing
+   * @returns an array of processed events
+   */
+  public async process(event: RawEvent): Promise<CWEvent<IEventData>[]> {
+    const kind = ParseType(event.event);
+    if (!kind) return [];
+    try {
+      const cwEvent = await Enrich(this._api, event.blockNumber, kind, event);
+      return [ cwEvent ];
+    } catch (e) {
+      log.error(`Failed to enrich event: ${e.message}`);
+      return [];
+    }
+  }
+}
