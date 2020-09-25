9817ff17a63deadd4e14feef8f8f6b3bd3f67246
zakhap <zhs2104@columbia.edu>
2020-09-25 12:35:34 -0700

    Fix interfaces , => ;

--- src/marlin/types.ts
+++ src/marlin/types.ts
@@ -13,9 +13,9 @@ export type Proposal = UnPromisify<ReturnType<GovernorAlpha['functions']['propos
 
 // API is imported contracts classes
 interface IMarlinContracts {
-  comp: Comp,
-  governorAlpha: GovernorAlpha,
-  timelock: Timelock, 
+  comp: Comp;
+  governorAlpha: GovernorAlpha;
+  timelock: Timelock;
 };
 
 export type Api = IMarlinContracts;
@@ -58,116 +58,116 @@ type Balance = string;
 
 // Comp Event Interfaces
 export interface IApproval extends IEvent {
-  kind: EventKind.Approval,
-  owner: Address,
-  spender: Address,
-  amount: Balance,
+  kind: EventKind.Approval;
+  owner: Address;
+  spender: Address;
+  amount: Balance;
 }
 
 export interface IDelegateChanged extends IEvent {
-  kind: EventKind.DelegateChanged,
-  delegator: Address,
-  fromDelegate: Address,
-  toDelegate: Address,
+  kind: EventKind.DelegateChanged;
+  delegator: Address;
+  fromDelegate: Address;
+  toDelegate: Address;
 }
 
 export interface IDelegateVotesChanged extends IEvent {
-  kind: EventKind.DelegateVotesChanged,
-  delegate: Address,
-  previousBalance: Balance,
-  newBalance: Balance,
+  kind: EventKind.DelegateVotesChanged;
+  delegate: Address;
+  previousBalance: Balance;
+  newBalance: Balance;
 }
 
 export interface ITransfer extends IEvent {
-  kind: EventKind.Transfer,
-  from: Address,
-  to: Address,
-  amount: Balance,
+  kind: EventKind.Transfer;
+  from: Address;
+  to: Address;
+  amount: Balance;
 }
 
 // TODO: GovernorAlpha Event Interfaces
 export interface IProposalCanceled extends IEvent {
-  kind: EventKind.ProposalCanceled,
-  id: number,
+  kind: EventKind.ProposalCanceled;
+  id: number;
 }
 
 export interface IProposalCreated extends IEvent {
-  kind: EventKind.ProposalCreated,
-  id: number,
-  proposer?: Address,
-  targets?: Address[], // string[]
-  values: string[], // BigNumberish[]
-  signatures: Address[], // string[]
-  calldatas: string[], // Arrayish[], TODO: decide on type
-  startBlock: number,
-  endBlock: number,
-  description: string,
+  kind: EventKind.ProposalCreated;
+  id: number;
+  proposer?: Address;
+  targets?: Address[]; // string[]
+  values: string[]; // BigNumberish[]
+  signatures: Address[]; // string[]
+  calldatas: string[]; // Arrayish[], TODO: decide on type
+  startBlock: number;
+  endBlock: number;
+  description: string;
 }
 
 export interface IProposalExecuted extends IEvent {
-  kind: EventKind.ProposalExecuted,
-  id: number,
+  kind: EventKind.ProposalExecuted;
+  id: number;
 }
 
 export interface IProposalQueued extends IEvent {
-  kind: EventKind.ProposalQueued,
-  id: number,
-  eta: number,
+  kind: EventKind.ProposalQueued;
+  id: number;
+  eta: number;
 }
 
 export interface IVoteCast extends IEvent {
-  kind: EventKind.VoteCast,
-  voter: Address,
-  proposalId: number,
-  support: boolean, 
-  votes: Balance, // TODO: or just number?
+  kind: EventKind.VoteCast;
+  voter: Address;
+  proposalId: number;
+  support: boolean,;
+  votes: Balance; // TODO: or just number?
 }
 
 
 // TODO: Timelock Event Interfaces
 export interface ICancelTransaction extends IEvent {
-  kind: EventKind.CancelTransaction,
-  txHash: string, // Arrayish, @jake suggested strings for hashes
-  target: Address,
-  value: Balance,// TODO: or just number?
-  signature: string,
-  data: string, // Arrayish
-  eta: number,
+  kind: EventKind.CancelTransaction;
+  txHash: string; // Arrayish, @jake suggested strings for hashes
+  target: Address;
+  value: Balance;// TODO: or just number?
+  signature: string;
+  data: string; // Arrayish
+  eta: number;
 }
 
 export interface IExecuteTransaction extends IEvent {
-  kind: EventKind.ExecuteTransaction,
-  txHash: string, // Arrayish, @jake suggested strings for hashes
-  target: Address,
-  value: Balance,// TODO: or just number?
-  signature: string,
-  data: string, // Arrayish
-  eta: number,
+  kind: EventKind.ExecuteTransaction;
+  txHash: string; // Arrayish, @jake suggested strings for hashes
+  target: Address;
+  value: Balance;// TODO: or just number?
+  signature: string;
+  data: string; // Arrayish
+  eta: number;
 }
 
 export interface INewAdmin extends IEvent {
-  kind: EventKind.NewAdmin,
-  newAdmin: Address,
+  kind: EventKind.NewAdmin;
+  newAdmin: Address;
 }
 
 export interface INewDelay extends IEvent {
-  kind: EventKind.NewDelay,
-  newDelay: number,
+  kind: EventKind.NewDelay;
+  newDelay: number;
 }
 
 export interface INewPendingAdmin extends IEvent {
-  kind: EventKind.NewPendingAdmin,
-  newPendingAdmin: Address,
+  kind: EventKind.NewPendingAdmin;
+  newPendingAdmin: Address;
 }
 
 export interface IQueueTransaction extends IEvent {
-  kind: EventKind.QueueTransaction,
-  txHash: string, // Arrayish, @jake suggested strings for hashes
-  target: Address,
-  value: Balance,// TODO: or just number?
-  signature: string,
-  data: string, // Arrayish, String | ArrayLike<number>
-  eta: number,
+  kind: EventKind.QueueTransaction;
+  txHash: string; // Arrayish, @jake suggested strings for hashes
+  target: Address;
+  value: Balance;// TODO: or just number?
+  signature: string;
+  data: string; // Arrayish, String | ArrayLike<number>
+  eta: number;
 }
 
 export type IEventData =
