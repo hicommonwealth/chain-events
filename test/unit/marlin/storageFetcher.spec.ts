19d40ce1ed463d94209a77e301a5b51761a95372
zakhap <zhs2104@columbia.edu>
2020-11-30 14:36:54 -0800

    New test still failing, but inside storage fetcher, ugh

--- test/unit/marlin/storageFetcher.spec.ts
+++ test/unit/marlin/storageFetcher.spec.ts
@@ -4,11 +4,12 @@ import { EventKind, Proposal } from '../../../src/marlin/types';
 import { Comp } from '../../../eth/types/Comp';
 import { GovernorAlpha, } from '../../../eth/types/GovernorAlpha';
 import { Timelock } from '../../../eth/types/Timelock';
+import { BigNumber } from 'ethers/utils';
 
 
 const { assert } = chai;
 
-const makeApi = (proposals?: Proposal[]) => {
+const makeApi = (proposals: Proposal[]) => {
   const comp = {
     provider: {
       getBlock: async (n: number) => ({ timestamp: n * 1000 }),
@@ -18,7 +19,7 @@ const makeApi = (proposals?: Proposal[]) => {
   const governorAlpha = {
     votingDelay: async () => '2',
     votingPeriod: async () => '2',
-    proposalCount: async () => '0',
+    proposalCount: async () => proposals.length,
     proposals: async (n: number) => proposals[n],
     provider: {
       getBlock: async (n: number) => ({ timestamp: n * 1000 }),
@@ -53,12 +54,29 @@ const makeDater = (minAvailableBlock = 0) => {
 
 describe('Marlin Storage Fetcher Tests', () => {
   it('should run gracefully with nothing in storage', async () => {
-    const api = makeApi();
+    const api = makeApi([]);
     const fetcher = new StorageFetcher(api, makeDater());
     const fetched = await fetcher.fetch();
     assert.deepEqual(fetched, []);
   });
   it('should handle an event from the contract', async () => {
-    
+    const proposals: Proposal[] = [{
+      id: new BigNumber(1),
+      proposer: '',
+      eta: new BigNumber(3),
+      startBlock: new BigNumber(0),
+      endBlock: new BigNumber(3*172),
+      forVotes: new BigNumber(0),
+      againstVotes: new BigNumber(0),
+      canceled: false,
+      executed: false,
+      description: 'hello world',
+    } as unknown as Proposal]
+    const api = makeApi(proposals);
+    const fetcher = new StorageFetcher(api, makeDater());
+    console.log(await api.governorAlpha.proposalCount());
+    const fetched = await fetcher.fetch();
+    console.log('fetched:', fetched);
+    assert.deepEqual(fetched, []);
   });
 });
