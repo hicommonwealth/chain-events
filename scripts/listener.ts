ca9269094bca38eb61bba8b3d4ca87b281edbebf
Maxwell Foley <maxwell.foley@certik.org>
2021-02-25 18:35:19 -0500

    add clover finance

--- scripts/listener.ts
+++ scripts/listener.ts
@@ -1,11 +1,51 @@
 import * as yargs from 'yargs';
 
 import { spec as EdgewareSpec } from '@edgeware/node-types';
+import * as CloverSpec from '@clover-network/node-tpye';
 import {
   chainSupportedBy, IEventHandler, CWEvent, SubstrateEvents, MarlinEvents, MolochEvents, EventSupportingChains
 } from '../dist/index';
 
+
+const CloverSpec = {
+  types: {
+    Amount: 'i128',
+    Keys: 'SessionKeys3',
+    AmountOf: 'Amount',
+    Balance: 'u128',
+    CurrencyId: {
+      _enum: ['CLV', 'CUSDT', 'DOT', 'CETH']
+    },
+    CurrencyIdOf: 'CurrencyId',
+    CurrencyTypeEnum: {
+      _enum: ['CLV', 'CUSDT', 'DOT', 'CETH']
+    },
+    PairKey: 'u64',
+    Rate: 'FixedU128',
+    Ratio: 'FixedU128',
+    Price: 'FixedU128',
+    Share: 'u128',
+    OracleKey: 'CurrencyId',
+    CurrencyInfo: {
+      id: 'CurrencyId',
+      name: 'CurrencyTypeEnum'
+    },
+    ExchangeInfo: {
+      balance: 'Balance',
+      routes: 'Vec<CurrencyTypeEnum>'
+    },
+    PoolId: {
+      _enum: {
+        Swap: 'u64'
+      }
+    },
+    EcdsaSignature: '[u8; 65]',
+    EvmAddress: 'H160',
+  }
+}
+
 const networkUrls = {
+  'clover': 'wss://api.clover.finance',
   'edgeware': 'ws://mainnet1.edgewa.re:9944',
   'edgeware-local': 'ws://localhost:9944',
   'edgeware-testnet': 'wss://beresheet1.edgewa.re',
@@ -22,6 +62,7 @@ const networkUrls = {
 } as const;
 
 const networkSpecs = {
+  'clover': CloverSpec,
   'edgeware': EdgewareSpec,
   'edgeware-local': EdgewareSpec,
   'edgeware-testnet': EdgewareSpec,
