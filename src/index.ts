e5a40a909af2558be4ee6966deafe35f76de24fc
timolegros <timothee@ualberta.ca>
2021-07-19 20:35:16 +0200

    build files

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,9 @@ export * as SubstrateListener from './chains/substrate/Listener';
 export * as MolochListener from './chains/moloch/Listener';
 export * as MarlinListener from './chains/marlin/Listener';
 export * as Erc20Listener from './chains/erc20/Listener';
-// export { createListener, getRabbitMQConfig } from './util';
-// export * from './handlers';
-// export * from './rabbitmq/producer';
+export { createListener, getRabbitMQConfig } from './util';
+export * from './handlers';
+export * from './rabbitmq/producer';
 export * from './Listener';
 
 // defaults
