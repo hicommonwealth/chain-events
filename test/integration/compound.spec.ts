50650c52b09a3cc5b1471e0c0c43306403af07b3
Adam Lawrence <adam.ad.lawrence@gmail.com>
2021-09-30 11:37:48 -0400

    mod - fix alpha tests

--- test/integration/compound.spec.ts
+++ test/integration/compound.spec.ts
@@ -113,7 +113,7 @@ async function setupSubscription(subscribe = true): Promise<ISetupData> {
   const addresses: string[] = await provider.listAccounts();
   const [member, bridge] = addresses;
   const signer = provider.getSigner(member);
-  
+
   // Deploy contracts.
   const comp = await deployMPond(signer, member, bridge);
   const timelock = await deployTimelock(signer, member, 2 * 60); // 2 minutes delay
@@ -256,6 +256,7 @@ async function proposeAndVote(
       voter: from,
       support: voteYes,
       votes: voteWeight.toString(),
+      reason: undefined,
     });
   });
 }
@@ -319,7 +320,7 @@ async function proposeAndQueue(
 describe('Compound Event Integration Tests', () => {
   describe('COMP contract function events', () => {
     it('initial address should transfer tokens to an address', async () => {
-      const { comp, addresses, handler } = await setupSubscription();
+      const { comp, addresses } = await setupSubscription();
       // test volume
       const initialBalance = await comp.balanceOf(addresses[0]);
       expect(+initialBalance).to.not.be.equal(0);
